beb3cab5ce98a64ebb39f8adcdc49a98e3df2606
Kaique Correia <kaique.rc.tl@gmail.com>
2026-06-23 09:04:30 -0300

    feat: add auth route

--- app-backend/src/app.module.ts
+++ app-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { CompanyModule } from './modules/company/company.module';
 import { HealthModule } from './modules/health/health.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [HealthModule, UserModule, CompanyModule],
+  imports: [HealthModule, UserModule, CompanyModule, AuthModule],
   controllers: [],
   providers: [],
 })
